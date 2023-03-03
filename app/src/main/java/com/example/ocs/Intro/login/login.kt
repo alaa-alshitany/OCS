6b6d0e3c5a4995d24f57654d6622bb90f5a89ed7
alaa alshitany <alaa2alshitany@gmail.com>
2023-03-03 21:57:00 +0200

    login & register

--- app/src/main/java/com/example/ocs/Intro/login/login.kt
+++ app/src/main/java/com/example/ocs/Intro/login/login.kt
@@ -1,12 +1,25 @@
 package com.example.ocs.Intro.login
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.ocs.R
 
 class login : AppCompatActivity() {
+    lateinit var register_btn:Button
+    lateinit var activity: Activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        register_btn=findViewById(R.id.register_btn)
+        register_btn.setOnClickListener { moveToRegister()}
+    }
+
+    private fun moveToRegister() {
+        activity=this
+        startActivity(Intent(activity,register::class.java))
+       finish()
     }
 }
---
+++ app/src/main/java/com/example/ocs/Intro/login/register.kt
@@ -0,0 +1,14 @@
+package com.example.ocs.Intro.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ocs.R
+
+
+class register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+}
