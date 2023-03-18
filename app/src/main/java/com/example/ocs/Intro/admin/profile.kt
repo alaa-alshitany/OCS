3cd0317da6e4d5f5cd97ee7af7c797cba35de55e
alaa alshitany <alaa2alshitany@gmail.com>
2023-03-18 15:06:01 +0200

    app

---
+++ app/src/main/java/com/example/ocs/Intro/admin/profile.kt
@@ -0,0 +1,12 @@
+package com.example.ocs.Intro.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ocs.R
+
+class profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.admin_profile)
+    }
+}
