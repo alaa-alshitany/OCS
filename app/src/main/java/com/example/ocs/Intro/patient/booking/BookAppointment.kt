725229b544b109b348ec072b975bd6aa1fde60f8
basma195055 <basmatalaat758@gmail.com>
2023-04-28 21:35:59 +0300

    nav_bar of patient_pages

--- app/src/main/java/com/example/ocs/Intro/patient/booking/BookAppointment.kt
+++ app/src/main/java/com/example/ocs/Intro/patient/booking/BookAppointment.kt
@@ -1,22 +1,61 @@
 package com.example.ocs.Intro.patient.booking
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.ocs.R
+import com.google.android.material.navigation.NavigationView
 
 
 class BookAppointment : AppCompatActivity() {
+    // navigation bar
+    lateinit var toggle: ActionBarDrawerToggle
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_appoitnment)
         bindingItems()
 
+        //navigation bar
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle( this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nau_home-> Toast.makeText(applicationContext,"clicked Home", Toast.LENGTH_SHORT).show()
+                R.id.nau_profile-> Toast.makeText(applicationContext,"clicked Profile", Toast.LENGTH_SHORT).show()
+                R.id.nau_booking-> Toast.makeText(applicationContext,"clicked Booking", Toast.LENGTH_SHORT).show()
+                R.id.nau_logout-> Toast.makeText(applicationContext,"clicked Logout", Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
+
 
     }
+
+    //nav_bar
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun bindingItems(){
 
 
