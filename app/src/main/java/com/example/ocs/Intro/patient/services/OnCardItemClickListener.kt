9cd41284e34a1cf56bb3842c13e6873c8aa3d113
basma195055 <basmatalaat758@gmail.com>
2023-05-02 12:14:14 +0300

    onclick

---
+++ app/src/main/java/com/example/ocs/Intro/patient/services/OnCardItemClickListener.kt
@@ -0,0 +1,6 @@
+package com.example.ocs.Intro.patient.services
+
+interface OnCardItemClickListener {
+
+    fun onClick(c: serviceModel)
+}
---
+++ app/src/main/java/com/example/ocs/Intro/patient/services/OnItemRecycleClickListener.kt
@@ -0,0 +1,6 @@
+package com.example.ocs.Intro.patient.services
+
+interface OnItemRecycleClickListener {
+
+    fun onClick(c: serviceModel?)
+}
