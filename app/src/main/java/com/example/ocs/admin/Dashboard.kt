94d43a058ecb517cad088a72404c5c6bfd2280a1
basma195055 <basmatalaat758@gmail.com>
2023-05-04 20:07:39 +0300

    dashboard

--- app/src/main/java/com/example/ocs/admin/Dashboard.kt
+++ app/src/main/java/com/example/ocs/admin/Dashboard.kt
@@ -1,12 +1,36 @@
 package com.example.ocs.admin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ocs.R
+import com.example.ocs.databinding.ActivityDashboardBinding
+import com.example.ocs.patient.services.OnCardItemClickListener
 
-class Dashboard : AppCompatActivity() {
+class Dashboard : AppCompatActivity()  {
+
+    //listener on dashboard_card
+    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+
+        //listener on dashboard_card
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cardProf.setOnClickListener {
+            val cardprofileIntent = Intent(this, profile::class.java)
+            startActivity(cardprofileIntent)
+        }
+        binding.cardApp.setOnClickListener {
+            val cardappointmentIntent = Intent(this, recycle_request::class.java)
+            startActivity(cardappointmentIntent)
+        }
+        binding.cardDoc.setOnClickListener {
+            val carddoctorsIntent = Intent(this, Doctors::class.java)
+            startActivity(carddoctorsIntent)
+        }
     }
+
+
 }
