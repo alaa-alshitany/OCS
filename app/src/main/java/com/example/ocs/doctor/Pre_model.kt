ca85fc65cbf301950fb9230450d9f44ecbaaaad4
basma195055 <basmatalaat758@gmail.com>
2023-05-29 21:20:48 +0300

    upload file

---
+++ app/src/main/java/com/example/ocs/doctor/Pre_model.kt
@@ -0,0 +1,12 @@
+package com.example.ocs.doctor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ocs.R
+
+class Pre_model : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pre_model)
+    }
+}
