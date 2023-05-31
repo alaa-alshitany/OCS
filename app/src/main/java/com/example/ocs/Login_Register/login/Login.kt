6064c41532aec74d91c321ad3dd60df89f723d9e
alaa alshitany <alaa2alshitany@gmail.com>
2023-05-31 03:17:40 +0300

    app

--- app/src/main/java/com/example/ocs/Login_Register/login/Login.kt
+++ app/src/main/java/com/example/ocs/Login_Register/login/Login.kt
@@ -7,7 +7,6 @@
         import android.os.Build
         import androidx.appcompat.app.AppCompatActivity
         import android.os.Bundle
-        import android.util.Log
         import android.view.Menu
         import android.view.MenuItem
         import android.view.View
@@ -15,9 +14,6 @@
         import android.widget.EditText
         import android.widget.ImageView
         import android.widget.TextView
-        /////////////////////////////////////////////////////
-        import com.opencsv.RFC4180ParserBuilder
-        ////////////////////////////////////////////////////
         import android.widget.Toast
         import com.example.ocs.Login_Register.forgetPassword.ForgetPassword
         import com.example.ocs.Patient.PatientData
@@ -34,10 +30,6 @@
         import com.google.firebase.database.*
         import com.google.firebase.database.ktx.getValue
         import com.google.firebase.ktx.Firebase
-        //import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions
-        //import com.google.firebase.ml.common.modeldownload.FirebaseModelManager
-        //import com.google.firebase.ml.custom.*
-        import com.google.firebase.storage.StorageReference
 
         class Login : AppCompatActivity() {
             private lateinit var registerBtn:Button
@@ -130,167 +122,7 @@
                     }else {
                         Toast.makeText(applicationContext, R.string.internetError, Toast.LENGTH_LONG).show()
                     }
-                               /*
-                                val storageRef = FirebaseStorage.getInstance().getReference("Test_Data/")
-                                val file1Ref =storageRef.child("10172943.hkl")
-                                val localFile = File.createTempFile("temp", ".hkl")
-                                file1Ref.getFile(localFile).addOnSuccessListener {
-                                    // Read the contents of the file into a byte array
-                                    val bytes = localFile.readBytes()
-                                    val floatArray = FloatArray(75)
-                                    ByteBuffer.wrap(bytes).asFloatBuffer().get(floatArray)
-                                    for (i in floatArray.indices) {
-                                        if (floatArray[i].isNaN()) {
-                                            floatArray[i] = 1f
-                                        }
-                                    }
-                                    val drugFeatTensor = TensorBuffer.createFixedSize(intArrayOf(1, 1, 75), DataType.FLOAT32)
-                                    drugFeatTensor.loadArray(floatArray)
-
-                                    //val drugInput = extractNumericValuesFromCsvRow(file1Ref, intArrayOf(1, 697))
-                                    //  val drugInputTensor = TensorBuffer.createFixedSize(intArrayOf(1, 697), DataType.FLOAT32)
-                                    //  drugInputTensor.loadArray(drugInput)
-                                    //for (i in drugInput.indices) {
-                                    //     Log.d("druginput", "drugInput[$i]: ${drugInput[i]}")
-                                    //   }
-                                    ////////////////////////////////////////////////////////////////////////////////////
-                                    val file2Ref = storageRef.child("mutation.csv")
-                                    val mutInput = extractNumericValuesFromCsvRow(
-                                        file2Ref,
-                                        intArrayOf(1, 1, 34673, 1)
-                                    )
-                                    val mutInputTensor = TensorBuffer.createFixedSize(
-                                        intArrayOf(1, 1, 34673, 1),
-                                        DataType.FLOAT32
-                                    )
-                                    mutInputTensor.loadArray(mutInput)
-                                    for (i in mutInput.indices) {
-                                        Log.d("mutInput", "mutInput[$i]: ${mutInput[i]}")
-                                    }
-                                    //////////////////////////////////////////////////////////////////////////
-                                    val file3Ref = storageRef.child("genomic.csv")
-                                    val expInput = extractNumericValuesFromCsvRow(
-                                        file3Ref,
-                                        intArrayOf(1,697)
-                                    )
-                                    val expInputTensor = TensorBuffer.createFixedSize(
-                                        intArrayOf(1, 697),
-                                        DataType.FLOAT32
-                                    )
-                                    expInputTensor.loadArray(expInput)
-                                    ////////////////////////////////////////////////////////////////////////////////
-                                    val file4Ref = storageRef.child("methylation.csv")
-                                    val methyInput =
-                                        extractNumericValuesFromCsvRow(file4Ref, intArrayOf(1,808))
-                                    val methyInputTensor = TensorBuffer.createFixedSize(
-                                        intArrayOf(1, 808),
-                                        DataType.FLOAT32
-                                    )
-                                    methyInputTensor.loadArray(methyInput)
-                                    //////////////////////////////////////////////////////////////////////////////////
-                                    val file5Ref = storageRef.child("drug1.csv")
-                                    val drugInput = extractNumericValuesFromCsvRow(
-                                        file5Ref,
-                                        intArrayOf(1, 1, 1)
-                                    )
-                                    val drugInputTensor = TensorBuffer.createFixedSize(
-                                        intArrayOf(1, 1, 1),
-                                        DataType.FLOAT32
-                                    )
-                                    drugInputTensor.loadArray(drugInput)
-                                    //////////////////////////////////////////////////////////////////////////////////////
-                                    Log.d("TFLite version", TensorFlowLite.runtimeVersion())
-                                    /////////////////////////////////////////////////////////////////////////////////////
-                                    // Download and use the TensorFlow model
-                                    val conditions = CustomModelDownloadConditions.Builder()
-                                        .requireWifi()  // Also possible: .requireCharging() and .requireDeviceIdle()
-                                        .build()
-                                    lateinit var interpreter: Interpreter
-                                    FirebaseModelDownloader.getInstance().getModel(
-                                        "Deep_CDR",
-                                        DownloadType.LOCAL_MODEL_UPDATE_IN_BACKGROUND,
-                                        conditions
-                                    )
-                                        .addOnSuccessListener {
-                                            val modelFile = it?.file
-                                            if (modelFile != null) {
-                                                Log.d("this", "model downloaded successfully")
-                                                interpreter = Interpreter(modelFile)
-
-                                                for (i in 0..4) {
-                                                    Log.d(
-                                                        "input tensor $i shape",
-                                                        interpreter.getInputTensor(i).shape()
-                                                            .contentToString()
-                                                    )
-                                                    Log.d(
-                                                        "input tensor $i data type",
-                                                        interpreter.getInputTensor(i)
-                                                            .dataType().name
-                                                    )
-                                                    Log.d(
-                                                        "input tensor $i name",
-                                                        interpreter.getInputTensor(i).name()
-                                                    )
-                                                }
-                                                Log.d(
-                                                    "output tensor shape",
-                                                    interpreter.getOutputTensor(0).shape()
-                                                        .contentToString()
-                                                )
-                                                Log.d(
-                                                    "output tensor data type",
-                                                    interpreter.getOutputTensor(0).dataType().name
-                                                )
-
-
-                                                val outputShape =
-                                                    interpreter.getOutputTensor(0).shape()
-                                                val outputDataType =
-                                                    interpreter.getOutputTensor(0).dataType()
-                                                val outputBuffer =
-                                                    TensorBuffer.createFixedSize(
-                                                        outputShape,
-                                                        outputDataType
-                                                    )
-
-                                                val inputs = arrayOf<Any>(
-                                                    expInputTensor.buffer,
-                                                    drugFeatTensor.buffer,
-                                                    mutInputTensor.buffer,
-                                                    methyInputTensor.buffer,
-                                                    drugInputTensor.buffer,
-
-                                                    )
-                                                val outputs =
-                                                    mutableMapOf<Int, Any>(0 to outputBuffer.buffer)
-
-                                                interpreter.runForMultipleInputsOutputs(
-                                                    inputs,
-                                                    outputs
-                                                )
-                                                val output = outputBuffer.floatArray[0]
-                                                val roundedOutput = Math.round(output *1000.0)/1000.0
-                                                val outputBinary =
-                                                    if (roundedOutput >= 0.5) 1 else 0
-                                                if (outputBinary == 1) {
-                                                    Log.d(
-                                                        "Drug is Resistance for patient with percentage:  ",
-                                                        "${roundedOutput * 100} %"
-                                                    )
-                                                } else {
-                                                    Log.d(
-                                                        "Drug is Sensitive for patient with percentage:  ",
-                                                        "${roundedOutput * 100} %"
-                                                    )
-                                                }
-
-                                            }
-                                        }
-                                }// Run the model
-                } else {
-                    Log.d("this", "model failed to downloaded ")
-          */       }
+                                   }
             }
             private fun readDoctorData(email: String,password: String) {
                 val auth = Firebase.auth
@@ -326,30 +158,7 @@
                         }
                         }
                     }
-            private fun extractNumericValuesFromCsvRow(csvRef: StorageReference, shape: IntArray): FloatArray {
-            val task = csvRef.getBytes(Long.MAX_VALUE)
-            while (!task.isComplete); // wait until the task completes
-            if (task.isSuccessful) {
-            val csvByteArray = task.result
-            val csvData = csvByteArray.toString(Charsets.UTF_8)
-            val parser = RFC4180ParserBuilder().build() // create an RFC4180Parser
-            val row = parser.parseLine(csvData) // parse the first row from the CSV file
-            val numericValues = row.filter { it.trim().toFloatOrNull() != null } // filter out the non-numeric values
-            val floatArray = numericValues.map { it.trim().toFloat() }.toFloatArray() // convert the numeric values to a float array
-            val product = shape.reduce { acc, i -> acc * i } // calculate the product ofthe dimensions of the TensorBuffer shape
-            if (floatArray.size > product) { // check if the size of the float array is larger than the product of the dimensions of theTensorBuffer shape
-               Log.w("TensorBuffer warning", "The size of the float array returned by extractNumericValuesFromCsvRow is larger than the specified shape of the TensorBuffer. Truncating the array to fit the shape and padding the remaining elements with 0.")
-            }
-            val reshapedArray = FloatArray(product){1.0f}
-            floatArray.take(product).forEachIndexed { index, value ->
-               reshapedArray[index] = value
-            }
-            return reshapedArray // reshape the float array to match the TensorBuffer shape and return it
-            } else {
-            Log.e("FirebaseStorage", "Error retrieving CSV file: ${task.exception}")
-            return FloatArray(0) // return an empty float array if the task is not successful
-            }
-            }
+
             private fun adminLogin() {
             email_edt.setHint(R.string.a_email_hint)
             loginTextview.setText(R.string.a_login)
