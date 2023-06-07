5dd46b1b37b98d817bd3a97d19ad2de506d90bff
alaa alshitany <alaa2alshitany@gmail.com>
2023-06-07 17:12:41 +0300

    app

--- app/src/main/java/com/example/ocs/Admin/Dashboard/Dashboard.kt
+++ app/src/main/java/com/example/ocs/Admin/Dashboard/Dashboard.kt
@@ -1,10 +1,14 @@
 package com.example.ocs.Admin.Dashboard
 
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.example.ocs.Admin.Appointments.Appointments
 import com.example.ocs.Admin.Doctors.Doctors
@@ -13,33 +17,34 @@ import com.example.ocs.Login_Register.login.Login
 import com.example.ocs.Login_Register.login.Prefrences
 import com.example.ocs.R
 import com.example.ocs.databinding.ActivityDashboardBinding
-import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.charts.HorizontalBarChart
 import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.util.*
+import kotlin.collections.ArrayList
 
 class Dashboard : AppCompatActivity()  {
-
     lateinit var piechart: PieChart
-
-    //listener on dashboard_card
     private lateinit var binding: ActivityDashboardBinding
     private lateinit var pref: Prefrences
     private lateinit var context: Context
+    private lateinit var barChart:HorizontalBarChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar!!.elevation= 0F
         init()
+        barchartData()
         //listener on dashboard_card
-        binding = ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
         binding.profile.setOnClickListener {
             val cardProfileIntent = Intent(this, Profile::class.java)
             startActivity(cardProfileIntent)
@@ -53,71 +58,21 @@ class Dashboard : AppCompatActivity()  {
             startActivity(cardDoctorsIntent)
         }
         binding.logout.setOnClickListener {
-        pref.prefClear()
+            pref.prefClear()
             moveToLogin()
         }
 
-        // Get a reference to the Firebase Realtime Database
-        val database = FirebaseDatabase.getInstance()
-        val dataRef = database.getReference("Doctors")
-        val chart = findViewById<BarChart>(R.id.chart)
-
-        // Add a listener to retrieve the data from Firebase
-        dataRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Initialize a list to hold the BarEntry objects
-                val entries = ArrayList<BarEntry>()
-                // Loop through the data and add it to the entries list
-                for (snapshot in dataSnapshot.children) {
-                    val oncologytype = snapshot.child("specialization").value.toString()
-                    val doctors = snapshot.child("Doctors").value.toString().toFloat()
-                    entries.add(BarEntry(entries.size.toFloat(), doctors))
-                }
-                // Create a BarDataSet object with the data
-                val dataSet = BarDataSet(entries, "oncology type")
-                dataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
-                // Create a BarData object with the BarDataSet
-                val data = BarData(dataSet)
-                // Configure the chart view
-                chart.data=data
-                chart.xAxis.setDrawGridLines(false)
-                chart.xAxis.position = XAxis.XAxisPosition.BOTTOM
-                // Set the labels for the x-axis using the data from Firebase
-                val labels = ArrayList<String>()
-                for (snapshot in dataSnapshot.children) {
-                    val oncologytype = snapshot.child("specialization").value.toString()
-                    labels.add(oncologytype)
-                }
-                chart.xAxis.valueFormatter = IndexAxisValueFormatter(labels.toTypedArray())
-                chart.description.isEnabled = true
-                chart.legend.isEnabled = false
-                chart.axisLeft.isEnabled = true
-                chart.axisRight.isEnabled = false
-                chart.animateY(1000)
-                chart.invalidate()
-            }
-            override fun onCancelled(error: DatabaseError) {
-                // Handle errors
-            }
-        })
 
 
-
-
-
-
-
-
-
-      //bar_chart
+        //bar_chart
         /**val chart = findViewById<BarChart>(R.id.chart)
         val entries = listOf(
-            BarEntry(0f, 10f),
-            BarEntry(1f, 20f),
-            BarEntry(2f, 30f),
-            BarEntry(3f, 40f),
-            BarEntry(4f, 50f),
-            BarEntry(5f, 60f)
+        BarEntry(0f, 10f),
+        BarEntry(1f, 20f),
+        BarEntry(2f, 30f),
+        BarEntry(3f, 40f),
+        BarEntry(4f, 50f),
+        BarEntry(5f, 60f)
         )
         val dataSet = BarDataSet(entries, "Values")
         dataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
@@ -146,14 +101,168 @@ class Dashboard : AppCompatActivity()  {
         piechart.description.text="Pie Chart"
         piechart.centerText= "List"
         piechart.animateY(2000)
-
-
-
     }
-     private fun init() {
-         context = this
-         pref = Prefrences(context)
-     }
+
+private fun barchartData(){
+    val database = FirebaseDatabase.getInstance()
+    val dataRef = database.getReference("Doctors")
+
+    dataRef.addListenerForSingleValueEvent(object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val specializationCountMap = mutableMapOf<String, Int>()
+
+            for (doctorSnapshot in snapshot.children) {
+                val specialization = doctorSnapshot.child("specialization").value.toString()
+                specializationCountMap[specialization] = specializationCountMap.getOrDefault(specialization, 0) + 1
+            }
+            createHorizontalBarChart(specializationCountMap.keys.toList(), specializationCountMap.values.toList())
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            Log.e(TAG, "Failed to read value.", error.toException())
+        }
+    })
+}
+    @SuppressLint("ResourceAsColor")
+    private fun createHorizontalBarChart(labels: List<String>, counts: List<Int>) {
+        barChart.visibility=View.VISIBLE
+        val barEntries = mutableListOf<BarEntry>()
+        // Add each bar entry to the list
+        for (i in labels.indices) {
+            barEntries.add(BarEntry(i.toFloat(), counts[i].toFloat()))
+        }
+
+        // Set up the bar data set
+        val barDataSet = BarDataSet(barEntries, "Counts")
+        // Set a random color for each bar
+        val colors = mutableListOf<Int>()
+        for (i in labels.indices) {
+            colors.add(getRandomColor())
+        }
+        barDataSet.colors = colors
+
+        // Set up the x-axis labels and position
+        val xAxisLabels = labels.toTypedArray()
+        val xAxis = barChart.xAxis
+        xAxis.apply {
+            valueFormatter = IndexAxisValueFormatter(xAxisLabels)
+            position = XAxis.XAxisPosition.BOTTOM_INSIDE
+            setDrawGridLines(false)
+            setDrawAxisLine(false)
+            granularity = 1f
+            textSize = 10f
+            isGranularityEnabled = true
+            labelCount = labels.size // Set the number of labels to show
+            setValueFormatter(object : ValueFormatter() {
+                override fun getFormattedValue(value: Float): String {
+                    return labels[value.toInt()]
+                }
+            })
+        }
+            val extraLeftOffset = resources.getDimensionPixelSize(R.dimen.bar_chart_extra_left_offset)
+            val extraRightOffset = resources.getDimensionPixelSize(R.dimen.bar_chart_extra_right_offset)
+            barChart.setExtraOffsets(extraLeftOffset.toFloat(), 0f, extraRightOffset.toFloat(), 0f)
+
+        // Define a subclass of ValueFormatter that overrides the getFormattedValue() method with a lambda function
+        class LabelValueFormatter(private val labels: List<String>) : ValueFormatter() {
+            override fun getFormattedValue(value: Float): String {
+                return value.toInt().toString()
+            }
+            override fun getAxisLabel(value: Float, axis: AxisBase?): String {
+                return value.toInt().toString()
+            }
+        }
+
+        // Use the LabelValueFormatter subclass to set the value formatter for the y-axis labels
+        var yAxisLeft = YAxis().apply {
+            setDrawGridLines(false)
+            setDrawAxisLine(false)
+            textSize = 16f
+            setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+            setDrawLabels(true)
+            valueFormatter = LabelValueFormatter(labels)
+            axisMinimum = 0f // set minimum value to 0
+            axisMaximum = Math.ceil(counts.maxOrNull()?.toDouble() ?: 0.0).toInt().toFloat() // set maximum value to highest value rounded up to the nearest whole number
+            granularity = 1f // set interval to 1
+        }
+        val yAxisRight = YAxis().apply {
+            setDrawGridLines(false)
+            setDrawAxisLine(false)
+            textSize = 10f
+            setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
+            setDrawLabels(false)
+            valueFormatter = LabelValueFormatter(labels)
+        }
+
+        // Set up the bar data object
+        val barData = BarData(barDataSet).apply {
+            barWidth = 0.5f
+        }
+
+        barChart.apply {
+            data = barData
+            setDrawGridBackground(false)
+            setDrawBorders(false)
+            setScaleEnabled(false)
+            setTouchEnabled(false)
+            setPinchZoom(false)
+            setDoubleTapToZoomEnabled(false)
+            setDrawValueAboveBar(false)
+            xAxis.apply {
+                valueFormatter = IndexAxisValueFormatter(xAxisLabels)
+                position = XAxis.XAxisPosition.BOTTOM_INSIDE
+                setDrawGridLines(false)
+                setDrawAxisLine(false)
+                granularity = 1f
+                isGranularityEnabled = true
+            }
+
+            barDataSet.setAxisDependency(YAxis.AxisDependency.LEFT)
+            barChart.axisLeft.mAxisMinimum=0f
+            barChart.axisLeft.granularity=1f
+            barChart.axisLeft.mAxisRange=8f
+            axisRight.isEnabled = false
+            legend.isEnabled = false
+            description.isEnabled = true
+            description.text="Doctor's Specializations"
+            description.textSize= 10F
+            description.xOffset=100f
+            description.yOffset=-14f
+            animateXY(1000, 1000)
+            invalidate()
+        }
+    }
+    // Helper function to generate a random color
+    private fun getRandomColor(): Int {
+        val rnd = Random()
+        val minBrightness = 150 // Set the minimum brightness to 150
+        val minSaturation = 0.4f // Set the minimum saturation to 0.4
+        var r = rnd.nextInt(256)
+        var g = rnd.nextInt(256)
+        var b = rnd.nextInt(256)
+        var hsv = FloatArray(3)
+        Color.RGBToHSV(r, g, b, hsv)
+        var brightness = hsv[2] * 255
+        var saturation = hsv[1]
+        while (brightness < minBrightness || saturation < minSaturation) {
+            // Generate new random values until the color meets the brightness and saturation criteria
+            r = rnd.nextInt(256)
+            g = rnd.nextInt(256)
+            b = rnd.nextInt(256)
+            Color.RGBToHSV(r, g, b, hsv)
+            brightness = hsv[2] * 255
+            saturation = hsv[1]
+        }
+        return Color.argb(200, r, g, b)
+    }
+    private fun init() {
+        context = this
+        pref = Prefrences(context)
+        binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        barChart = binding.chart
+        barChart.visibility=View.INVISIBLE
+    }
     private fun moveToLogin() {
         startActivity(Intent(this, Login::class.java).putExtra("hint", R.string.a_email_hint.toString()))
         Toast.makeText(this, R.string.logout, Toast.LENGTH_LONG).show()
